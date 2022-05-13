988a61ec7c55d9e26c39a966994d9b98893ee0f3
emptyhead <korea6370@naver.com>
2022-05-13 18:01:08 +0900

    방금한 Recoder.py

--- Recoder.py
+++ Recoder.py
@@ -1,5 +1,9 @@
 import pyaudio
 import wave
+import numpy as np
+from pynput import keyboard
+from threading import Thread
+
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -7,7 +11,29 @@ CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 1
 WAVE_OUTPUT_FILENAME = "output.wav"
+NOISE_MININUM_VALUE = 250
 
+def on_press(key):
+    if key == keyboard.Key.esc:
+        print("Recording is finished.")
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+        exit()    
+    print("key : ", type(key), key)
+    
+def keyint():
+    with keyboard.Listener(
+        on_press=on_press,
+    ) as listener:
+        listener.join()
 
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT,
@@ -20,19 +46,15 @@ print("Start to record the audio.")
 
 frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-    
-print("Recording is finished.")
+# for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
 
-stream.stop_stream()
-stream.close()
-p.terminate()
-
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+Thread(target=keyint, daemon=False)
+while True:
+    data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
+    if(int(np.average(np.abs(data))) > NOISE_MININUM_VALUE):
+        # print(int(np.average(np.abs(data))))
+        frames.extend(data)
+        # print("length : ", len(frames))
+        if len(frames) > 10:
+            print(frames)
+            frames = []
