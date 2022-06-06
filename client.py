a96007e3c120e8c29ee0f34f5785e5e3f94a6938
emptyhead <korea6370@naver.com>
2022-06-06 09:21:49 +0900

    server, client update

--- client.py
+++ client.py
@@ -1,30 +1,104 @@
-import socket
-import select
-import sys
-from test import main
+from socket import *
+from threading import *
+import os
+import datetime
+import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 8000))
+#--------------클라이언트 세팅 -----------------------
+Host='127.0.0.1' # 서버의 IP주소를 입력하세요.
+Port = 9190 # 사용할 포트 번호. 
+#---------------------------------------
 
-name = None
+COLORS = [
+    '\033[31m', # RED
+    '\033[32m', # GREEN
+    '\033[33m', # YELLOW
+    '\033[34m', # BLUE
+    '\033[35m', # MAGENTA
+    '\033[36m', # CYAN
+    '\033[37m', # WHITE
+    '\033[4m',  # UNDERLINE
+]
+
+
 sentence = None
 
+def now_time(): 
+    now = datetime.datetime.now()
+    time_str=now.strftime('[%H:%M] ')
+    return time_str
+
+def send_func():
+    while True:
+        # send_data=input('나 : ')
+        send_data = input(f'{color}\033[36m Me : ')
+        client_sock.send(send_data.encode('utf-8'))
+        if send_data=='!quit':
+            print('연결을 종료하였습니다.')
+            break 
+    client_sock.close()
+    os._exit(1)
+
+def recv_func():
+    while True:
+        try:
+            recv_data=(client_sock.recv(1024)).decode('utf-8')
+            if len(recv_data)==0:
+                print('[SYSTEM] 서버와의 연결이 끊어졌습니다.')
+                client_sock.close()
+                os._exit(1)
+        except Exception as e:
+            print('예외가 발생했습니다.', e) # 예외처리중
+            print('[SYSTEM] 메시지를 수신하지 못하였습니다.')
+        else:
+            print(recv_data)
+            pass
+
+client_sock=socket(AF_INET, SOCK_STREAM)
+try:
+    client_sock.connect((Host,Port))
+
+except ConnectionRefusedError:
+    print('서버에 연결할 수 없습니다.')
+    print('1. 서버의 ip주소와 포트번호가 올바른지 확인하십시오.')
+    print('2. 서버 실행 여부를 확인하십시오.')
+    os._exit(1)
+
+except:
+    print('프로그램을 정상적으로 실행할 수 없습니다. 프로그램 개발자에게 문의하세요.')
+
+else:
+    print('[SYSTEM] 서버와 연결되었습니다.')
+
 while True:
-    read, write, fail = select.select((s, sys.stdin), (), ())
+    name = input('사용하실 닉네임을 입력하세요 :')     
+    if ' ' in name:
+        print('공백은 입력이 불가능합니다.')
+        continue
+    client_sock.send(name.encode())
+    is_possible_name=client_sock.recv(1024).decode()
     
-    for desc in read:
-        if desc == s:
-            data = s.recv(4096)
-            print(data.decode())
-            sentence = data.decode()
-            
-            if name is None:
-                name = data.decode()
-                s.send(f'{name} is connected!'.encode())
-            
-            else:
-                msg = desc.readline()
-                msg = msg.replace('\n', '')
-                s.send(f'{name} {msg}'.encode())
-                
-            test = main(sentence)
+    if is_possible_name=='yes':
+        print(now_time()+ '채팅방에 입장하였습니다.')
+        client_sock.send('!enter'.encode())
+        break
+
+    elif is_possible_name=='overlapped':
+        print('[SYSTEM] 이미 사용중인 닉네임입니다.')
+
+    elif len(client_sock.recv(1024).decode())==0:
+        print('[SYSTEM] 서버와의 연결이 끊어졌습니다.')
+        client_sock.close()
+        os._exit(1)
+
+color = COLORS[len(name) % len(COLORS)]
+sender=Thread(target=send_func, args=())
+receiver=Thread(target=recv_func, args=())
+sender.start()
+receiver.start()
+
+while True:
+    time.sleep(1)
+    pass
+
+client_sock.close()         
